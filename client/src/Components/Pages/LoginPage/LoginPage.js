e490a9eec94fbcb55ecea2f4165972ddaffa71b7
george-botezatu <george.botezatu@ibm.com>
2022-05-27 21:42:17 +0300

    colors

--- client/src/Components/Pages/LoginPage/LoginPage.js
+++ client/src/Components/Pages/LoginPage/LoginPage.js
@@ -2,8 +2,9 @@ import React from "react";
 import LoginForm from "../../Forms/LoginForm/LoginForm";
 
 const LoginPage = () => {
+	const componentClass = "login-page-container";
 	return (
-		<div>
+		<div className={componentClass}>
 			<LoginForm />
 		</div>
 	);
