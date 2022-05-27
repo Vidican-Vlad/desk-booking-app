e31ec6d32b371bdfdf9a984583c49017f8a1a05a
Gabriel Baraboi <gabrielbaraboi5@gmail.com>
2022-05-27 21:29:26 +0300

    created navbar; added fontawesome

--- client/src/Components/Navigation/Navbar/Navbar.js
+++ client/src/Components/Navigation/Navbar/Navbar.js
@@ -1,8 +1,38 @@
 import "./Navbar.scss";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	const { isConnected } = useSelector((state) => state.auth);
+
+	const classComponent = "navbar-container";
+	const logoClass = `${classComponent}__logo`;
+	const menuClass = `${classComponent}__menu`;
+	const menuListClass = `${menuClass}__list`;
+	const menuItemClass = `${menuListClass}__item`;
+
+	return (
+		<nav className={classComponent}>
+			<div className={logoClass}>
+				<a href="/">
+					<img src="/images/logo.png" alt="logo" />
+				</a>
+			</div>
+			<div className={menuClass}>
+				<ul className={menuListClass}>
+					<li className={menuItemClass}>
+						<a href="/">Home</a>
+					</li>
+					<li className={menuItemClass}>
+						<a href="/">About</a>
+					</li>
+					<li className={menuItemClass}>
+						<a href="/">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
