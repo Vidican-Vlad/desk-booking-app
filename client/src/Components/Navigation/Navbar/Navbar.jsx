6ea014869424542554f0d3fd729adb95326813a7
Gabriel Baraboi <gabrielbaraboi5@gmail.com>
2022-05-28 15:03:04 +0300

    sidebar

--- client/src/Components/Navigation/Navbar/Navbar.jsx
+++ client/src/Components/Navigation/Navbar/Navbar.jsx
@@ -8,19 +8,31 @@ import {
 	faMapLocationDot,
 	faArrowRightToBracket,
 	faArrowRightFromBracket,
+	faLocation,
+	faUsers,
+	faCircleInfo,
+	faChartLine
 } from "@fortawesome/free-solid-svg-icons";
 import { logout } from "../../../Redux/Features/authenticationSlice";
 import { deleteCookies } from "../../../Utils/UtilFunctions";
 
 const Navbar = () => {
 	const { isConnected } = useSelector((state) => state.auth);
+	const { isAdmin } = useSelector((state) => state.auth);
 	const dispatch = useDispatch();
 	const classComponent = "navbar-container";
 	const logoClass = `${classComponent}__logo`;
 	const menuClass = `${classComponent}__menu`;
+	const menuHeaderClass = `${menuClass}__header`;
 	const menuListClass = `${menuClass}__list`;
 	const menuItemClass = `${menuListClass}__item`;
 	const navbarDividerClass = `${classComponent}__divider`;
+	const navbarUserClass = `${classComponent}__user`;
+	const navbarUserAvatarClass = `${navbarUserClass}__avatar`;
+	const navbarUserInfoClass = `${navbarUserClass}__info`;
+	const navbarUserInfoName = `${navbarUserInfoClass}__name`;
+	const navbarUserInfoEmail = `${navbarUserInfoClass}__email`;
+	const navbarUserInfoRole = `${navbarUserInfoClass}__role`;
 
 	return (
 		<nav className={classComponent}>
@@ -32,6 +44,7 @@ const Navbar = () => {
 			</div>
 			<div className={navbarDividerClass}></div>
 			<div className={menuClass}>
+				<span className={menuHeaderClass}>General</span>
 				<ul className={menuListClass}>
 					<li className={menuItemClass}>
 						<Link to="/">
@@ -41,18 +54,60 @@ const Navbar = () => {
 					</li>
 					<li className={menuItemClass}>
 						<Link to="/">
-							<FontAwesomeIcon icon={faHouse} />
-							About
+							<FontAwesomeIcon icon={faLocation} />
+							Locations
 						</Link>
 					</li>
 					<li className={menuItemClass}>
 						<Link to="/">
-							<FontAwesomeIcon icon={faHouse} />
-							Contact
+							<FontAwesomeIcon icon={faUsers} />
+							Users
+						</Link>
+					</li>
+					<li className={menuItemClass}>
+						<Link to="/">
+							<FontAwesomeIcon icon={faCircleInfo} />
+							About
 						</Link>
 					</li>
 				</ul>
 			</div>
+			{ isAdmin && (
+				<>
+					<div className={navbarDividerClass}></div>
+					<span className={menuHeaderClass}>Admin</span>
+					<ul className={menuListClass}>
+						<li className={menuItemClass}>
+							<Link to="/dashboard">
+								<FontAwesomeIcon icon={faChartLine} />
+								Dashboard
+							</Link>
+						</li>
+					</ul>
+				</>
+			)}
+			{isConnected ? (
+			<>			
+				<div className={navbarDividerClass}></div>
+				<span className={menuHeaderClass}>Welcome</span>
+				<div className={navbarUserClass}>
+					<div className={navbarUserAvatarClass}>
+
+					</div>
+					<div className={navbarUserInfoClass}>
+						<span className={navbarUserInfoName}>
+							FirstName LastName
+						</span>
+						<span className={navbarUserInfoEmail}>
+							email@mail.com
+						</span>
+						<span className={navbarUserInfoRole}>
+							{ isAdmin ? "Admin" : "User" }
+						</span>
+					</div>
+				</div>
+			</>
+			) : null}
 			<div className={navbarDividerClass}></div>
 			<div className={menuClass}>
 				<ul className={menuListClass}>
