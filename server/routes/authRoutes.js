6919ad237a619f46886c8df46d8162def45c1314
Vlad-Vidican <vlad.vidican00@e-uvt.ro>
2022-05-28 11:22:52 +0300

    Auth implemented

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/authController");
+const { addOwnAccToReq, checkIFEmailAlreadyUsed, checkIfAdmin, validateLogin, validateRegisterReq  } = require("../middleware/UserMiddleware")
+const auth = require("../middleware/auth");
+
 
 //REGISTER
-router.post("/register", controller.registerUser);
+// registerAdmin is just for development purposes
+router.post("/registerAdmin", checkIFEmailAlreadyUsed, validateRegisterReq, controller.registerAdmin);
+
+router.post("/register", auth, addOwnAccToReq, checkIfAdmin, checkIFEmailAlreadyUsed, validateRegisterReq, controller.registerUser )
 
 //LOGIN
-router.post("/login", controller.loginUser);
+router.post("/login", validateLogin, controller.loginUser);
 
 module.exports = router;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const controller = require("../controllers/userController");
 const auth = require("../middleware/auth");
 
+
 // GET ALL USERS
 router.get("/users", auth, controller.getAllUsers);
 
