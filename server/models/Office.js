9525019c8d1dfca4267589f246a217ff3beaa164
Vlad-Vidican <vlad.vidican00@e-uvt.ro>
2022-05-28 18:12:20 +0300

    stuff

--- server/models/Office.js
+++ server/models/Office.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 var OfficeSchema = new mongoose.Schema({
     Address: { type: String, required: true},
     Name: { type: String, required: true},
+    Telephone: {type: String, required: true}
 });
 
 
